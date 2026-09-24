7d472b79ecfdebcfc20d06bbde75195dce8d1c7b
KhaledAbuMustafa <khaled.abu-mustafa@web.de>
2026-09-24 18:32:07 +0200

    Delete secrets.example.h

--- secrets.example.h
+++
@@ -1,7 +0,0 @@
-// Template: copy this file, rename it to "secrets.h" and enter your own credentials.
-// secrets.h is listed in .gitignore and will not be uploaded to GitHub.
-#pragma once
-
-#define WIFI_SSID     "YOUR_WIFI"
-#define WIFI_PASSWORD "YOUR_PASSWORD"
-#define OTA_PASSWORD  "CHOOSE_AN_OTA_PASSWORD"   // protects over-the-air updates
